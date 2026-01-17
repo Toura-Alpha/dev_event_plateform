28ed5f491c5cedebd95fc3d01460a199f770ff73
Toura-Alpha <steronenombiekenne@gmail.com>
2026-01-17 17:28:38 +0100

    add Navbar and EventCard components; implement featured events section

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted_grotesk",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martinMono.variable} min-h-screen antialiased`}
       >
+        <Navbar />
         <div className="absolute inset-0 to-0 z-[-1] min-h-screen">
           <LightRays
             raysOrigin="top-center-offset"
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ExploreBtn from "../components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import events from "@/lib/constants";
 
 const page = () => {
   return (
@@ -10,7 +12,19 @@ const page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in One Place{" "}
       </p>
+
       <ExploreBtn />
+
+      <div className="mt-20 space-y-7 mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32 2xl:mx-40">
+        <h3>Featured Events</h3>
+        <u className="events ">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </u>
+      </div>
     </section>
   );
 };
