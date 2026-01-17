5bfa9afb3d31ed8772fa511d15a6c3d85e1d6bfe
Toura-Alpha <steronenombiekenne@gmail.com>
2026-01-17 08:30:29 +0100

    complete header for the home page
    Code for the home page header with it's design and components

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ExploreBtn from "../components/ExploreBtn";
+
+const page = () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can&#39;t Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place{" "}
+      </p>
+      <ExploreBtn />
+    </section>
+  );
+};
+
+export default page;
