b1cbb7d41936ed1a9ad347c158ef276037d73266
Toura-Alpha <steronenombiekenne@gmail.com>
2026-01-20 15:30:22 +0100

    implement database models

---
+++ database/index.ts
@@ -0,0 +1,5 @@
+export { EventModel } from './event.model';
+export type { Event, EventDocument } from './event.model';
+
+export { BookingModel } from './booking.model';
+export type { Booking, BookingDocument } from './booking.model';
